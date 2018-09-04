51743afba89eb1b9681185c35b43ee50bf4f040e
jhunnel <jhunnel@adspark.ph>
2018-09-04 15:10:57 +0800

    debug receiver

--- receiver.php
+++ receiver.php
@@ -9,8 +9,10 @@
 define('SITE_ROOT', __DIR__);
 require 'vendor/autoload.php';
 
-$str = 'message=BxPS+87C82220&message_type=incoming&mobile_number=639455712578&request_id=4f2a7dd0-af1c-11e8-aadd-8bff7ed8ffb0&shortcode=29290633&timestamp=1535939675';
+$post_data = file_get_contents('php://input');
+
+$str = 'message=EDN+87C82220&message_type=incoming&mobile_number=639455712578&request_id=4f2a7dd0-af1c-11e8-aadd-8bff7ed8ffb0&shortcode=29290633&timestamp=1535939675';
 
 $route = new \KeywordRouter\ReceiverRouter($str);
 $route->createTransaction();
-$route->deploy();
+//$route->deploy();
